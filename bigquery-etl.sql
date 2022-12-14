418ec8ddb31ee35d20619656a58cc00d71217ebd
ridwansukri <93005475+ridwansukri@users.noreply.github.com>
2022-12-15 00:33:21 +0800

    Update bigquery-etl.sql

--- bigquery-etl.sql
+++ bigquery-etl.sql
@@ -29,14 +29,3 @@ CREATE TABLE `bike_dataset.bike_trip` AS (
     FROM `bike_dataset.joined_dataset` AS a
     ORDER BY started_at DESC
 );
-
-/* join weather table to bike_trip table */
-CREATE OR REPLACE TABLE `acoustic-portal-322707.bike_dataset.bike_trip` AS (
-SELECT a.*, b.T2M, b.RH2M, b.PRECTOTCORR, b.WS10M
-FROM `acoustic-portal-322707.bike_dataset.bike_trip` AS a
-JOIN `acoustic-portal-322707.bike_dataset.weather_chicago_beta`  AS b
-ON a.YEAR = b.YEAR AND
-   a.MO = b.MO AND
-   a.DY = b.DY AND
-   a.HR = b.HR
-);
